77dba38ee3972de75648f86b472be03b90e4f882
detteiu2001 <akiaki.gintama@gmail.com>
2023-05-22 14:56:45 +0900

    Initial Commit

---
+++ ShWatchSample/ShWatchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShWatchSampleApp.swift
+//  ShWatchSample
+//
+//  Created by でっていう2001 on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ShWatchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
